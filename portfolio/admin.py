aa3aa510773ead7d2f74a609773cd7dc5564c289
Titus Soporan <tsopor@gmail.com>
2011-02-08 14:52:00 -0500

    More verbosity in admin for models, some documentation and template fixes.

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from portfolio.models import Client, Skill, Project, ProjectResource
 
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
 class ResourceInline(admin.StackedInline):
     model = ProjectResource
     extra = 3
 
 class ProjectAdmin(admin.ModelAdmin):
     inlines = [ResourceInline]
+    list_display = ('name', 'slug', 'url', 'created', 'description')
 
 admin.site.register(Client)
-admin.site.register(Skill)
+admin.site.register(Skill, SkillAdmin)
 admin.site.register(Project, ProjectAdmin)
     
