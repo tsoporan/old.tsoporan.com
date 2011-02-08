6534199002be0317b5326d3976b2b3fbce278e1a
Titus Soporan <tsopor@gmail.com>
2011-02-08 15:18:46 -0500

    Mapped out urls to views.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('', 
-    url(r'^$', 'portfolio.views.index', name='index'),
+    url(r'^$', 'portfolio.views.index', name="index"),
+    url(r'^about/$', 'portfolio.views.about', name="about"),
+    url(r'^work/$', 'portfolio.views.work', name="work"),
+    url(r'^contact/$', 'portfolio.views.contact', name="contact"),
 )
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
 from portfolio.models import Project
+from portfolio.forms import ContactForm
+from django.views.generic.simple import direct_to_template
+from django.views.generic.list_detail import object_list, object_detail
 
 def index(request, template="portfolio/index.html"):
-    return render_to_response(template, {}, context_instance=RequestContext(request))
+    context = {
+        'projects': Project.objects.all(),
+    }
+    return direct_to_template(request, template, extra_context=context)
+
+def about(request, template='portfolio/about.html'):
+    return direct_to_template(request, template)
+
+def work(request, template='portfolio/work.html'):
+    pass
+
+def contact(request, template='portfolio/contact.html'):
+    pass
