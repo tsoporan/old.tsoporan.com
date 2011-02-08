33b00dcf1de8f6d520c837178f7c5d863b9adc59
Titus Soporan <tsopor@gmail.com>
2011-02-08 12:54:37 -0500

    Finalized models.

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,32 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+class Client(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    
+    class Meta:
+        ordering = ['name']
+    
+    def __unicode__(self):
+        return self.name
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    def __unicode__(self):
+        return self.name
+
 class Project(models.Model):
-    name = models.CharField(max_length=250, help_text="Name of the project.", unique=True)
+    name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    client = models.ForeignKey(Client)
+    skills = models.ManyToManyField(Skill)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('portfolio.views.project_detail', (), {'slug': self.slug),})
 
     def save(self):
         if not self.slug:
@@ -13,6 +36,14 @@ class Project(models.Model):
     def __unicode__(self):
         return self.name
 
+
+class ProjectResource(models.Model):
+    resource = models.FileField(upload_to = "")
+    description = models.TextField()    
+    project = models.ForeignKey(Project)
+
+    def __unicode__(self):
+        return self.resource.name
     
 
 
