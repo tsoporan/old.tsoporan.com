003c45a23e873a5e43fc246acbad5a138c20b107
Titus Soporan <tsopor@gmail.com>
2011-02-08 16:54:58 -0500

    Basic layout and styles.

--- urls.py
+++ urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.defaults import *
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'', include('portfolio.urls')), 
     (r'^admin/', include(admin.site.urls)),
+
+    #For development only:
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}), 
 )
